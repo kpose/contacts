61446e7676c56883bfda96e6e60ba9e503291e8a
kpose <ganihujude@gmail.com>
2019-08-26 11:42:27 +0100

    Load contact from API

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Contacts from './screens/Contacts';
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <Contacts />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
