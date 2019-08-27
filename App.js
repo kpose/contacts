1056e9164236d72d0b66aafb6bcba8cd8ffb6f7d
kpose <ganihujude@gmail.com>
2019-08-27 07:35:14 +0100

    Add profile screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Contacts from './screens/Contacts';
+/*import Contacts from './screens/Contacts';*/
+import Profile from './screens/Profile';
 
 
 export default function App() {
   return (
-    <Contacts />
+    <Profile />
   )
 }
 
